b94d533a04be47a5767a86288a589543b479fa89
Antonio Saraiva <antonioguilher2010@gmail.com>
2026-04-15 12:32:24 -0300

    Add files via upload

---
+++ Ex  1 introducao java ag7/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int numero = 10;
+        System.out.println("o numero é" +numero);
+    }
+}
---
+++ Ex 2 introducao java ag7/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int  a;
+        float  b;
+        double  c;
+        a = 1;
+        b = 2;
+        c =3;
+        System.out.println("valor de a é"+a);
+        System.out.println("valor de b é"+b);
+        System.out.println("valor de c é"+c);
+
+    }
+    }
